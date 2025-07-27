d95fbfafc4afb03b32addfbe45b744fae8356c24
aqib-ali-jp <aqibbrohi12@gmail.com>
2025-07-27 16:32:39 +0500

    operator,Cases,data types, naming rules

--- operators/index.js
+++ operators/index.js
@@ -1,43 +1,43 @@
 // assignment  operator '='
 
-const category = 'action'
+// const category = 'action'
 
-// for addition
-const a = 5;
-const b = 7;
+// // for addition
+// const a = 5;
+// const b = 7;
 
-const c = a + b;
-console.log("The value of C is:",c)
-// for substraction
+// const c = a + b;
+// console.log("The value of C is:", c)
+// // for substraction
 
-const num1 = 5;
-const num2 = 7;
+// const num1 = 5;
+// const num2 = 7;
 
-const result = num1 - num2;
-console.log("The Result is:",result)
+// const result = num1 - num2;
+// console.log("The Result is:", result)
 
-// for multiplication
+// // for multiplication
 
-const d = 5;
-const e = 7;
+// const d = 5;
+// const e = 7;
 
-const h = d * e;
-console.log("The Result of h is:",h)
+// const h = d * e;
+// console.log("The Result of h is:", h)
 
 // exponent **
 
-const exp = 7**3
-console.log(exp)
+// const exp = 7 ** 3
+// console.log(exp)
 
 // division /
 
-const div = 10/2
-console.log(div)
+// const div = 10 / 2
+// console.log(div)
 
 // reminder(modulus) % 
 
-const mod = 13%5
-console.log(mod)
+// const mod = 13 % 5
+// console.log(mod)
 
 // increment ++
 // let x = 5;
@@ -52,9 +52,9 @@ console.log(mod)
 // console.log(x)
 
 // concatination
-const k = 6
-const z = '11'
-console.log(k + z) // -5
+// const k = 6
+// const z = '11'
+// console.log(k + z) // -5
 
 
 // comparision operator
@@ -90,9 +90,66 @@ console.log(k + z) // -5
 
 
 // let b1 = '' //empty string , 0 , false this means falsy values
-// console.log( b1 ? 'hello' : 'mello')
+// console.log( b1 ? 'hello' : 'helllo')
 
 
-let age = 18
-let p1 = age >= 18 ? "Eligigble for driving license" : "not eligible"
-console.log(p1)
+// let age = 18
+// let p1 = age >= 18 ? "Eligigble for driving license" : "not eligible"
+// console.log(p1)
+
+// let x = 5
+// let y = ++x
+// console.log(x)
+// console.log(y)
+
+// let veg = ''
+// if (veg) {
+//     console.log("Vegs are available in store")
+// } else {
+//     console.log("Vegs are not available in store")
+// }
+
+// Data types
+
+// JavaScript has 8 Datatypes
+// String let str = 'string'
+// Number let num = 10
+// Bigint let bgint = bigint("12123127361278617836127878")
+// Boolean let bool = false
+// Undefined let undef = undefine
+// Null let nullish = null
+// Symbol let user = symbol() used for uniqueness
+// Object let obj = {}
+
+// let str = 'string'
+// console.log(typeof str)
+// let num = 10
+// console.log(typeof num)
+// let bgint = BigInt("12123127361278617836127878")
+// console.log(typeof bgint)
+// let bool = true
+// console.log(typeof bool)
+// let undef = undefined
+// console.log(typeof undef)
+// let nullish = null
+// console.log(typeof nullish)
+
+// let aksdh = 'gjhg'
+// let alpha = aksdh
+// console.log(alpha)
+// console.log(typeof alpha)
+
+// name rules
+// let l2ol2 = 'error'
+// console.log(2lol)
+
+
+// let function = 'jjsj';
+// console.log(function)
+
+// JS Case Sensitive
+// let yousuf = 'A good person'
+// console.log(yousuF)
+
+// let userName;
+// let userImage;
